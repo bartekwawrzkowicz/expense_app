db8c3255d2430046f209579738b110148601e7ab
Bartosz <73258197+bartekwawrzkowicz@users.noreply.github.com>
2021-09-14 21:31:48 +0200

    building expenseItem component

--- src/App.js
+++ src/App.js
@@ -3,9 +3,17 @@ import ExpenseItem from './components/ExpenseItem';
 
 
 const App = () => {
+  const expenses = [
+    { id: 1, title: 'Car Insuranse', amount: 294.73, date: new Date(2021, 8, 14), },
+    { id: 2, title: 'Alkohol', amount: 54.24, date: new Date(2021, 8, 10), },
+    { id: 3, title: 'Cigarettes', amount: 102.86, date: new Date(2021, 4, 21), },
+  ]
+
+
   return (
     <div className="main-column">
-      Main App component
+      Expenses
+      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
       <ExpenseItem />
     </div>
   );
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,30 @@
 import React from 'react';
+import '../styles/ExpenseItem.scss';
 
-const ExpenseItem = () => {
+const ExpenseItem = props => {
+
+    const day = props.date.toLocaleString('en-US', { month: 'long' });
+    const month = props.date.toLocaleString('en-US', { day: '2-digit' });
+    const year = props.date.getFullYear();
+
+    console.log(day, month, year)
+
+
+
     return (
         <div className="expense-item main-column">
             <div className="expense-item__date">
-                Date
-                </div>
-            <div className="expense-item__title">
-                <h2 className="expense-item__title-heading">Title</h2>
+                <div></div>
+                <div></div>
+                <div></div>
             </div>
-            <div className="expense-item__amount">
-                Amount
-                </div>
+            <div className="expense-item__title">
+                <h2 className="expense-item__title-heading">
+                    {props.title}
+                </h2>
+            </div>
+            <div className="expense-item__price">$
+            </div>
         </div>
     );
 }
