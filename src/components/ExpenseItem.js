5c1465a0433390287cc2e6f9e8c7b4eaaf934d17
Bartosz <73258197+bartekwawrzkowicz@users.noreply.github.com>
2021-09-15 15:28:30 +0200

    adding state via hook useState

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,26 @@
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import '../styles/ExpenseItem.scss';
 
 const ExpenseItem = props => {
+
+    const [title, setTitle] = useState(props.title);
+
+    const buttonHandler = () => {
+        setTitle('nowy title');
+    }
 
     return (
         <div className="expense-item main-column">
             <ExpenseDate date={props.date} />
             <div className="expense-item__title">
                 <h2 className="expense-item__title-heading">
-                    {props.title}
+                    {title}
                 </h2>
             </div>
             <div className="expense-item__price">{props.amount} PLN
             </div>
+            <button onClick={buttonHandler}>Change</button>
         </div>
     );
 }
