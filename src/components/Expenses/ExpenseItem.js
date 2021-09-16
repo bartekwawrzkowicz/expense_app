0f9ab4b7805a3c35f270318065be5e8ae7a8024b
Bartosz <73258197+bartekwawrzkowicz@users.noreply.github.com>
2021-09-16 17:08:13 +0200

    changing ExpenseItem to state-less component

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,26 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ExpenseDate from './ExpenseDate';
 import '../../styles/ExpenseItem.scss';
 
 const ExpenseItem = props => {
-
-    const [title, setTitle] = useState(props.title);
-
-    const buttonHandler = () => {
-        setTitle('nowy title');
-    }
 
     return (
         <div className="expense-item main-column">
             <ExpenseDate date={props.date} />
             <div className="expense-item__title">
                 <h2 className="expense-item__title-heading">
-                    {title}
+                    {props.title}
                 </h2>
             </div>
             <div className="expense-item__price">{props.amount} PLN
             </div>
-            <button onClick={buttonHandler}>Change</button>
         </div>
     );
 }
